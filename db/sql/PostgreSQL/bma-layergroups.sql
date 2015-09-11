87290aad7e47b6796541d5c6981d83b5d9ee0745
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-09-11 07:55:36 +0300

    Add lakes from Syke

--- db/sql/PostgreSQL/bma-layergroups.sql
+++ db/sql/PostgreSQL/bma-layergroups.sql
@@ -1,1 +1,2 @@
 INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Biomassa-atlas"},sv:{name:"Biomassa-atlas"},en:{name:"Biomassa-atlas"}}');
+INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Syke"},sv:{name:"Syke"},en:{name:"Syke"}}');
