857807c7f7f5ed5d7e28740b530002f66fe30fe7
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-09-16 07:50:51 +0300

    Enable MML basemap and make most layers non-default

---
+++ db/sql/PostgreSQL/bma-fix-default-view.sql
@@ -0,0 +1,6 @@
+-- It is only possible to define "selectedLayers" in the view configuration. This is a workaround to make most of them not selected.
+-- TODO: Extend Oskari to allow adding layers without making them selected
+
+update portti_view_bundle_seq set state =
+'{"east":"517620","selectedLayers":[{"id":' || (select id from oskari_maplayer where name = 'osm_finland:osm-finland') || '}]}'
+where bundleinstance = 'mapfull';
