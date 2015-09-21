8f35e45cef0e8c38a57734f9d278afac4f37c619
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-09-21 12:15:39 +0300

    Move non-default layers from view to setup.
    This requires the latest snapshot code from nls-oskari/oskari-server

--- db/sql/PostgreSQL/bma-fix-default-view.sql
+++
@@ -1,6 +0,0 @@
--- It is only possible to define "selectedLayers" in the view configuration. This is a workaround to make most of them not selected.
--- TODO: Extend Oskari to allow adding layers without making them selected
-
-update portti_view_bundle_seq set state =
-'{"east":"517620","selectedLayers":[{"id":' || (select id from oskari_maplayer where name = 'osm_finland:osm-finland') || '}],"north":"6874042","zoom":1}'
-where bundleinstance = 'mapfull';
