c13a3754fc5c4c3513245aaf505e443491b5ba8c
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-09-28 12:33:59 +0300

    Initial code for biomass measurement tool

--- WebContent/Oskari/bundles/bma/testbundle/instance.js
+++ WebContent/Oskari/bundles/bma/testbundle/instance.js
@@ -1,34 +1,163 @@
 /**
  * @class Oskari.bma.bundle.testbundle.TestbundleInstance
  *
- * This bundle does nothing but displays an alert
+ * This bundle provides a tool for biomass calculations on a user provided polygon
  */
 Oskari.clazz.define("Oskari.bma.bundle.testbundle.TestbundleInstance",
 
-    /**
-     * @method create called automatically on construction
-     * @static
-     */
+/**
+ * @method create called automatically on construction
+ * @static
+ */
 
-    function () {}, {
-        /**
-         * @method start
-         * BundleInstance protocol method
-         */
-        start: function () {
-            //alert('Tämä biomassa-atlaksen testi-bundle toimii!');
-        },
+function() {
+	this.sandbox = null;
+}, {
+	/**
+	 * @static
+	 * @property __name
+	 */
+	__name : 'TestBundle',
 
-        /**
-         * @method stop
-         * BundleInstance protocol method
-         */
-        stop: function () {},
-        /**
-         * @method update
-         * BundleInstance protocol method
-         */
-        update: function () {}
-    }, {
-        protocol: ['Oskari.bundle.BundleInstance']
-    });
+	/**
+	 * @method getName
+	 * @return {String} the name for the component
+	 */
+	getName : function() {
+		return this.__name;
+	},
+	/**
+	 * @method getSandbox
+	 */
+	getSandbox : function() {
+		return this.sandbox;
+	},
+	/**
+	 * @method init
+	 * implements Module protocol init method - initializes request handlers and templates
+	 */
+	init : function() {
+		return null;
+	},
+	/**
+	 * @method start
+	 * BundleInstance protocol method
+	 */
+	start : function() {
+		var me = this;
+
+		// Should this not come as a param?
+		var conf = me.conf;
+		var sandboxName = (conf ? conf.sandbox : null) || 'sandbox';
+		var sandbox = Oskari.getSandbox(sandboxName);
+		this.sandbox = sandbox;
+		// register to sandbox as a module
+		sandbox.register(me);
+
+		me._registerTools();
+
+		// register to listening events
+		for ( var p in me.eventHandlers) {
+			if (p) {
+				sandbox.registerForEventByName(me, p);
+			}
+		}
+	},
+
+	_toolButtonClicked : function() {
+		// here you can insert code that needs to be run exactly once after toolbar button has been clicked
+	},
+	
+	/**
+	 * Requests the tools to be added to the toolbar.
+	 * 
+	 * @method registerTool
+	 */
+	_registerTools : function() {
+		var me = this, request, tool, sandbox = this.getSandbox();
+		// Is button available or already added the button?
+		if (me._buttonsAdded) {
+			return;
+		}
+
+		me.buttons = {
+			'bmacalculator' : {
+				iconCls : 'tool-measure-area',
+				tooltip : "Laske biomassa alueella",
+				sticky : true,
+				callback : function() {
+					me._toolButtonClicked();
+				}
+			}
+		};
+
+		var reqBuilder = sandbox
+				.getRequestBuilder('Toolbar.AddToolButtonRequest');
+		if (!reqBuilder) {
+			// Couldn't get the request, toolbar not loaded
+			return;
+		}
+		request = reqBuilder("bmacalculator", "basictools",
+				me.buttons['bmacalculator']);
+		sandbox.request(me.getName(), request);
+		me._buttonsAdded = true;
+	},
+
+	/**
+	 * @method stop BundleInstance protocol method
+	 */
+	stop : function() {
+		var me = this;
+		var sandbox = this.sandbox;
+
+		for ( var p in me.eventHandlers) {
+			if (p) {
+				sandbox.unregisterFromEventByName(me, p);
+			}
+		}
+		me.sandbox.unregister(me);
+		me.started = false;
+	},
+	
+	/**
+	 * @method update
+	 * BundleInstance protocol method
+	 */
+	update : function() {
+	},
+	
+	/**
+	 * @method onEvent
+	 * Module protocol method/Event dispatch
+	 */
+	onEvent : function(event) {
+		var me = this, handler = me.eventHandlers[event.getName()];
+		if (!handler) {
+			return;
+		}
+		return handler.apply(this, [ event ]);
+	},
+	
+	/**
+	 * @static
+	 * @property eventHandlers
+	 * Best practices: defining which
+	 * events bundle is listening and how bundle reacts to them
+	 */
+	eventHandlers : {
+		'Toolbar.ToolSelectedEvent' : function(event) {
+			var me = this;
+			var sandbox = this.getSandbox();
+
+			/* we'll show prompt if measure tool has been selected */
+			if (event.getToolId() == 'bmacalculator') {
+				var msg = "Valitse alue, jonka biomassa lasketaan";
+				sandbox.request(me, sandbox.getRequestBuilder(
+						'ShowMapMeasurementRequest')(msg || "", false, null,
+						null));
+			}
+		}
+	}
+}, {
+	protocol : [ 'Oskari.bundle.BundleInstance' ]
+});
