8292c8dfd1e4b424b009ad834cea0f6e88b71c78
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-09-29 13:06:39 +0300

    points to polygon

--- src/main/java/fi/nls/oskari/BiomassCalculationController.java
+++ src/main/java/fi/nls/oskari/BiomassCalculationController.java
@@ -1,6 +1,7 @@
 package fi.nls.oskari;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -26,10 +27,29 @@ public class BiomassCalculationController {
     @RequestMapping(value="area", method=RequestMethod.POST)
     public Map<?, ?> calculateBiomassForArea(@RequestBody Map<?, ?> requestBody) {
         HashMap<String, Object> result = new HashMap<>();
-        Long theNumber = jdbcTemplate.queryForList("SELECT count(*) FROM biomass_data", Long.class).get(0);
+        String polygonAsWkt = polygonToWkt((List<Map<?,?>>) requestBody.get("points"));
+        String sql = "SELECT st_area(st_geomfromtext(?, 3067))";
+        Double theNumber = jdbcTemplate.queryForList(sql, new Object[]{polygonAsWkt}, Double.class).get(0);
         // TODO implementation
         result.put("test", theNumber);
         return result;
+    }
+    
+    private String polygonToWkt(List<Map<?, ?>> points) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("POLYGON((");
+        boolean first = true;
+        for (Map<?, ?> point : points) {
+            if (!first) {
+                sb.append(", ");
+            }
+            first = false;
+            sb.append(point.get("x"));
+            sb.append(" ");
+            sb.append(point.get("y"));
+        }
+        sb.append("))");
+        return sb.toString();
     }
     
 }
