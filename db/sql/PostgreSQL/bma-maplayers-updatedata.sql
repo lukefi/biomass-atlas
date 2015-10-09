32859272b4c0a61607cb79d3c08c369c01dc5f13
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-10-09 13:14:49 +0300

    These are now done with "bmatools sync-attributes-to-oskari-layers"

--- db/sql/PostgreSQL/bma-maplayers-updatedata.sql
+++
@@ -1,3 +0,0 @@
-UPDATE oskari_maplayer mlayer
-SET legend_image = (SELECT url||'?request=GetLegendGraphic&version=1.1.1&service=WMS&format=image/png&width=20&height=20&layer='||name FROM oskari_maplayer mlayer2 WHERE mlayer2.id=mlayer.id)
-WHERE groupid > 1;
