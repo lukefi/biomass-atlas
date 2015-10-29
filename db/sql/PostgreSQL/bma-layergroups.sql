68ebf9b77c12140c657e7c325fb44e19a00ef272
Petri Seppala <petri.seppala@mtt.fi>
2015-10-29 10:21:22 +0200

    Modified: oskari_layergroup name is changed from Biomassa-atlas to Luonnonvarakeskus, in bma-layergroups.sql

--- db/sql/PostgreSQL/bma-layergroups.sql
+++ db/sql/PostgreSQL/bma-layergroups.sql
@@ -1,3 +1,3 @@
-INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Biomassa-atlas"},sv:{name:"Biomassa-atlas"},en:{name:"Biomassa-atlas"}}');
+INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Luonnonvarakeskus"},sv:{name:"Luonnonvarakeskus"},en:{name:"Luonnonvarakeskus"}}');
 INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Syke"},sv:{name:"Syke"},en:{name:"Syke"}}');
 INSERT INTO oskari_layergroup (locale) values ('{ fi:{name:"Maanmittauslaitos"},sv:{name:"Maanmittauslaitos"},en:{name:"Maanmittauslaitos"}}');
