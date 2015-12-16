b66efc8f3a6c1530ede484009251ba7f2d5e2c10
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-12-16 14:29:00 +0200

    Add code to determine the nearest road

--- src/main/java/fi/luke/bma/service/GeometryService.java
+++ src/main/java/fi/luke/bma/service/GeometryService.java
@@ -1,6 +1,9 @@
 package fi.luke.bma.service;
 
+import java.util.NoSuchElementException;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
@@ -19,9 +22,45 @@ public class GeometryService {
     }
     
     public String getCircle(Point point, float radius) {
-        String sql = "SELECT ST_ASTEXT(the_geom) FROM (SELECT ST_Buffer(ST_MakePoint(" + point.getX() + "," + point.getY() + ", 3067)," + radius*1000 + ")) AS foo(the_geom)";
+        String sql = "SELECT ST_ASTEXT(the_geom) FROM (SELECT ST_Buffer(" + toPoint(point) + "," + radius*1000 + ")) AS foo(the_geom)";
         Query query = entityManager.createNativeQuery(sql);
         return (String) query.getSingleResult();
+    }
+    
+    public String toPoint(Point point) {
+        return "st_setsrid(st_point(" + point.getX() + "," + point.getY() + "), 3067)";
+    }
+    
+    public int getNearestRoad(Point point) {
+        int boxR = 500;
+        while (boxR < 3000000) {
+            String boundingBox = "st_setsrid(ST_GeomFromText('POLYGON(("
+                    + (point.getX()-boxR) + " "
+                    + (point.getY()-boxR) + ","
+                    + (point.getX()-boxR) + " "
+                    + (point.getY()+boxR) + ","
+                    + (point.getX()+boxR) + " "
+                    + (point.getY()+boxR) + ","
+                    + (point.getX()+boxR) + " "
+                    + (point.getY()-boxR) + ","
+                    + (point.getX()-boxR) + " "
+                    + (point.getY()-boxR) + "))'), 3067)";
+            String sql = "SELECT ogc_fid FROM roadsegment r WHERE st_intersects(r.geometry, " + boundingBox + ")"
+                    + " ORDER BY st_distance(r.geometry, " + toPoint(point) + ") ASC LIMIT 1";
+            Query query = entityManager.createNativeQuery(sql);
+            try {
+                return (Integer) query.getSingleResult();
+            }
+            catch (NoResultException e) {
+                boxR *= 2;
+            }
+        }
+        throw new NoSuchElementException("Could not find any roads near the given point");
+    }
+    
+    public String getRoadBuffer(Point point, float radius) {
+        int nearestRoad = getNearestRoad(point);
+        return null; // TODO
     }
     
 }
