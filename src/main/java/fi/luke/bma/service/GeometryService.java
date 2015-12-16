2258b740eaf3708b9b71e65ac641278676a72c06
Harri Pitkänen <harri.pitkanen@luke.fi>
2015-12-16 14:59:01 +0200

    Query for retrieving reachable roads

--- src/main/java/fi/luke/bma/service/GeometryService.java
+++ src/main/java/fi/luke/bma/service/GeometryService.java
@@ -31,7 +31,7 @@ public class GeometryService {
         return "st_setsrid(st_point(" + point.getX() + "," + point.getY() + "), 3067)";
     }
     
-    public int getNearestRoad(Point point) {
+    private Object[] getNearestRoad(Point point) {
         int boxR = 500;
         while (boxR < 3000000) {
             String boundingBox = "st_setsrid(ST_GeomFromText('POLYGON(("
@@ -45,11 +45,11 @@ public class GeometryService {
                     + (point.getY()-boxR) + ","
                     + (point.getX()-boxR) + " "
                     + (point.getY()-boxR) + "))'), 3067)";
-            String sql = "SELECT ogc_fid FROM roadsegment r WHERE st_intersects(r.geometry, " + boundingBox + ")"
+            String sql = "SELECT ogc_fid, startnode, endnode FROM roadsegment r WHERE st_intersects(r.geometry, " + boundingBox + ")"
                     + " ORDER BY st_distance(r.geometry, " + toPoint(point) + ") ASC LIMIT 1";
             Query query = entityManager.createNativeQuery(sql);
             try {
-                return (Integer) query.getSingleResult();
+                return (Object[]) query.getSingleResult();
             }
             catch (NoResultException e) {
                 boxR *= 2;
@@ -58,8 +58,23 @@ public class GeometryService {
         throw new NoSuchElementException("Could not find any roads near the given point");
     }
     
+    private String reachableNodesQuery(int nodeId, float radius) {
+        return "select id1 as node from pgr_drivingdistance('select ogc_fid as id, startnode as source, endnode as target, "
+                + "length::float8 as cost from roadsegment', " + nodeId + ", " + radius + ", false, false)";
+    }
+    
     public String getRoadBuffer(Point point, float radius) {
-        int nearestRoad = getNearestRoad(point);
+        Object[] nearestRoad = getNearestRoad(point);
+        int roadId = (int) nearestRoad[0];
+        int startNode = (int) nearestRoad[1];
+        int endNode = (int) nearestRoad[2];
+        String reachableRoadsSql =
+                "with reachable_nodes as ("
+                + reachableNodesQuery(startNode, radius * 1000) + " union "
+                + reachableNodesQuery(endNode, radius * 1000) + ")"
+                + "select r.ogc_fid from roadsegment r "
+                + "where (r.startnode in (select node from reachable_nodes) and r.endnode in (select node from reachable_nodes)) "
+                + "or r.ogc_fid = " + roadId;
         return null; // TODO
     }
     
