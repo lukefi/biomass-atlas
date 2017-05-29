4f4ce9110c5803b47a5c22df0e96fe237a4de188
Anil Maharjan <anil.maharjan@luke.fi>
2017-05-29 09:32:07 +0300

    #2797 Welcome window text is changed.

--- WebContent/Oskari/bundles/bma/welcometour/resources/locale/fi.js
+++ WebContent/Oskari/bundles/bma/welcometour/resources/locale/fi.js
@@ -17,11 +17,11 @@ Oskari.registerLocalization(
             },
             "page1": {
                 "title": "Tervetuloa testaamaan Biomassa-atlasta",
-                "message" : "Biomassa-atlas on metsä-, pelto-, lanta- ja jätebiomassojen paikkatiedon katselu-, analysointi ja raportointipalvelu. Voit tarkastella ja valita biomassatietotasoja vasemmalta sivupalkista löytyvästä BIOMASSAT-valikosta. VALITUT TASOT toiminnon kautta löydät lisätietoja biomassoista ja pääset säätämään karttatasojen läpinäkyvyyttä ja järjestystä. Vasemmasta palkista löytyvät myös karttaselitteet sekä työkalut alueiden rajaamiseen ja biomassojen laskentaan sekä haku-toiminto paikan etsimiseen kartalta. <br><br>" +
+                "message" : "Biomassa-atlas on metsä-, pelto-, lanta- ja jätebiomassojen paikkatiedon katselu-, analysointi ja raportointipalvelu. Voit tarkastella ja valita biomassatietotasoja vasemmalta sivupalkista löytyvästä BIOMASSAT-valikosta. VALITUT TASOT -toiminnon kautta löydät lisätietoja biomassoista ja pääset säätämään karttatasojen läpinäkyvyyttä ja järjestystä. Vasemmasta palkista löytyvät myös KARTTASELITTEET sekä ALUEVALINTA-työkalut alueiden rajaamiseen ja biomassojen laskentaan sekä HAKU-toiminto paikan etsimiseen kartalta. <br><br>" +
                 			"Biomassojen määrät perustuvat maastohavaintojen, mittausten, rekisteriaineistojen, satelliittikuvien ja kirjallisuuden perusteella muodostettuihin malleihin ja ovat perusteltuja arvioita muodostuvista määristä ja saatavilla olevasta potentiaalista.",
                 "listtitle" : "Näin käytät Biomassa-atlasta",
                 "listitem1" : "Valitse haluamasi jakeet BIOMASSAT-valikosta",
-                "listitem2" : "Piirrä tai valitse alue ALUEVALINNAT-kuvakkeiden työkaluilla ja laske tulokset",
+                "listitem2" : "Piirrä tai valitse alue ALUEVALINTA-kuvakkeiden työkaluilla ja laske tulokset",
                 "imagealt" : "Aluevalinta",
                 "tip" : "Vinkki: Sulkemalla ja avaamalla taustakartan uudelleen TAUSTAKARTAT-valikossa, saat nostettua kartan nopeasti päällimmäiseksi"
             },
