724ab1633f7ee5222db17ab32cdc6b4fe6dc4a77
Anil Maharjan <anil.maharjan@luke.fi>
2017-06-12 16:32:27 +0300

    #2932 Modified welcome text.

--- WebContent/Oskari/bundles/bma/welcometour/resources/locale/en.js
+++ WebContent/Oskari/bundles/bma/welcometour/resources/locale/en.js
@@ -15,7 +15,7 @@ Oskari.registerLocalization({
             "label": "Do not show this message again"
         },
         "page1": {
-            "title": "Tervetuloa testaamaan Biomassa-atlasta",
+            "title": "Tervetuloa käyttämään Biomassa-atlasta",
             "message" : "Biomassa-atlas on metsä-, pelto-, lanta- ja jätebiomassojen paikkatiedon katselu-, analysointi ja raportointipalvelu. Voit tarkastella ja valita biomassatietotasoja vasemmalta sivupalkista löytyvästä BIOMASSAT-valikosta. VALITUT TASOT -toiminnon kautta löydät lisätietoja biomassoista ja pääset säätämään karttatasojen läpinäkyvyyttä ja järjestystä. Vasemmasta palkista löytyvät myös KARTTASELITTEET sekä ALUEVALINTA-työkalut alueiden rajaamiseen ja biomassojen laskentaan sekä HAKU-toiminto paikan etsimiseen kartalta. <br><br>" +
             			"Biomassojen määrät perustuvat maastohavaintojen, mittausten, rekisteriaineistojen, satelliittikuvien ja kirjallisuuden perusteella muodostettuihin malleihin ja ovat perusteltuja arvioita muodostuvista määristä ja saatavilla olevasta potentiaalista.",
             "listtitle" : "Näin käytät Biomassa-atlasta",
