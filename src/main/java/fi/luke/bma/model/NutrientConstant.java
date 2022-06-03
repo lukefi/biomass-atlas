495348e0ebc48a8987d9f2e9aafa94816bb509e0
Anil Maharjan <anil.maharjan@luke.fi>
2022-06-03 14:11:40 +0300

    refactor: add remaining attributes of NutrientConstant model

--- src/main/java/fi/luke/bma/model/NutrientConstant.java
+++ src/main/java/fi/luke/bma/model/NutrientConstant.java
@@ -12,12 +12,32 @@ import fi.rktl.common.model.NonInsertableEntityWithLongId;
 @Table(name = "nutrient_constant")
 public class NutrientConstant extends NonInsertableEntityWithLongId {
     
+	@Column(name = "ts")
     private Double TS;  // Total Solid percent
     
     @Column(name = "nutrient_type")
     private String nutrientType;
     
+    @Column(name = "value")
     private Double value;   // Nutrient Coefficient value
+    
+    @Column(name = "fm")
+    private Double FM;
+    
+    @Column(name = "nutrient_id")
+    private Long nutrientId;
+    
+    @Column(name = "wms_style")
+    private String wmsStyle;
+    
+    @Column(name = "fi")
+    private String nameFI;
+    
+    @Column(name = "en")
+    private String nameEN;
+    
+    @Column(name = "sv")
+    private String nameSV;
 
     @OneToOne
     @JoinColumn(name = "attribute_id")
@@ -46,7 +66,56 @@ public class NutrientConstant extends NonInsertableEntityWithLongId {
     public void setValue(Double value) {
         this.value = value;
     }
-    public Attribute getAttribute() {
+    
+	public Double getFM() {
+		return FM;
+	}
+
+	public void setFM(Double fM) {
+		FM = fM;
+	}
+
+	public Long getNutrientId() {
+		return nutrientId;
+	}
+
+	public void setNutrientId(Long nutrientId) {
+		this.nutrientId = nutrientId;
+	}
+
+	public String getWmsStyle() {
+		return wmsStyle;
+	}
+
+	public void setWmsStyle(String wmsStyle) {
+		this.wmsStyle = wmsStyle;
+	}
+
+	public String getNameFI() {
+		return nameFI;
+	}
+
+	public void setNameFI(String nameFI) {
+		this.nameFI = nameFI;
+	}
+
+	public String getNameEN() {
+		return nameEN;
+	}
+
+	public void setNameEN(String nameEN) {
+		this.nameEN = nameEN;
+	}
+
+	public String getNameSV() {
+		return nameSV;
+	}
+
+	public void setNameSV(String nameSV) {
+		this.nameSV = nameSV;
+	}
+
+	public Attribute getAttribute() {
         return attribute;
     }
     
